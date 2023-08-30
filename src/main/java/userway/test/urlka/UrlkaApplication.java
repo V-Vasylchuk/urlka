a487c2a0c9fba07f45b4ccd7aac4e44888164079
Vasyl Vasylchuk <v.vasylchuk18@gmail.com>
2023-08-30 18:26:55 +0300

    initial commit

---
+++ src/main/java/userway/test/urlka/UrlkaApplication.java
@@ -0,0 +1,13 @@
+package userway.test.urlka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UrlkaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/userway/test/urlka/UrlkaApplicationTests.java
@@ -0,0 +1,13 @@
+package userway.test.urlka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
